98caa3059dd231e251ec2853e1228a776feaa0b2
chain_liu <chain_liu@compal.com>
2025-10-13 13:49:20 +0800

    2025/10/13_13:49

--- Platforms/Windows/App.xaml.cs
+++ Platforms/Windows/App.xaml.cs
@@ -11,15 +11,15 @@ namespace HWDPortalMaui.WinUI
     public partial class App : MauiWinUIApplication
     {
         /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
+        /// Initializes the singleton application object. This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
         public App()
         {
-            this.InitializeComponent();
+
+                this.InitializeComponent();
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
     }
-
 }
